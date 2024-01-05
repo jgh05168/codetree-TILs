8692d841d82ef89c346da1e7e71daf60c70a15db
전규훈 <97810918+jgh05168@users.noreply.github.com>
2024-01-05 15:16:53 +0900

    Title: 원소 10개의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240105/원소 10개의 합/sum-of-10-elements.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    
+    int sum = 0;
+    int arr[11] = {0, };
+
+
+    for (int i=0;i<10;i++){
+        cin >> arr[i];
+        sum += arr[i];
+    }
+
+    cout << sum;
+    return 0;
+}
