6534a4e9cd4320d6fcd60578f4061bcabc55017c
Gyuhun Jeon <97810918+jgh05168@users.noreply.github.com>
2024-08-25 12:57:07 +0900

    Title: 승자독식 모노폴리, Time: 240ms, Memory: 26MB, Status: Passed - CodeTree

--- 240825/승자독식 모노폴리/odd-monopoly.py
+++ 240825/승자독식 모노폴리/odd-monopoly.py
@@ -1,184 +1,131 @@
-DIR_NUM = 4
-EMPTY = (401, 401)
-EMPTY_NUM = 401
+'''
+n x n
+1. 턴이 한 번 진행할 때 각 플레이어들 한 칸 씩 이동
+    - 해당 칸에 이동했을 때, 플레이어는 해당 칸을 독점 계약한다.
+    - 초기 땅 역시 해당 플레이어의 땅
+    1-1. 독점 계약은 k턴동안만 유효하다.
+        - k턴이 지나면, 해당 칸은 다시 주인이 없는 땅이 된다.
+2. 각 플레이어는 방향 별로 이동 우선순위를 갖는다.
+    2-1. 인접한 상하좌우 4칸 중 아무도 독점계약을 맺지 않은 칸으로 이동
+    2-2. 만약 그런 칸이 없는 경우, 인접 칸 중본인이 독점계약한 땅으로 이동
+    2-3. 이동할 수 있는 칸이 여러개라면, 이동 우선순위를 따진다.
+    - 플레이어가 보고 있는 방향은 그 직전에 이동한 방향임
+3. 모든 플레이어가 이동한 후 한 칸에 여러 플레이어가 있을 경우, 가장 작은 플레이어만 살아남고, 나머진 죽는다
 
-# 변수 선언 및 입력:
-n, m, k = tuple(map(int, input().split()))
-given_map = [
-    list(map(int, input().split()))
-    for _ in range(n)
-]
-next_dir = [
-    [
-        [0 for _ in range(DIR_NUM)]
-        for _ in range(DIR_NUM)
-    ]
-    for _ in range(m + 1)
-]
+주의 :
+- 한 칸에 여러 명이 있는 경우, 살아남은 자가 독점 계약을 한다. min()으로 판단하기
+-
 
-player = [
-    [EMPTY for _ in range(n)]
-    for _ in range(n)
-]
-next_player = [
-    [EMPTY for _ in range(n)]
-    for _ in range(n)
-]
+순서 :
+1. 각 플레이어 이동
+    - min 사용
+2. 살아남은 자들에 대해서 땅 업데이트
+3. 땅 정보 한 칸씩 줄여주기
 
-contract = [
-    [EMPTY for _ in range(n)]
-    for _ in range(n)
-]
+종료 조건 : 1번 플레이어만 살아남기까지 걸리는 턴의 개수
+'''
 
-elapsed_time = 0
+import sys
+input = sys.stdin.readline
 
-
-def in_range(x, y):
-    return 0 <= x and x < n and 0 <= y and y < n
+dr = [0, -1, 1, 0, 0]
+dc = [0, 0, 0, -1, 1]
 
 
-def can_go(x, y, target_num):
-    if not in_range(x, y):
-        return False
-    
-    # target 번호와 contract 번호가 일치한 
-    # 경우에만 이동이 가능합니다.
-    contract_num, _ = contract[x][y]
-    return contract_num == target_num
+def move_players():
+    new_grid = [[[] * n for _ in range(n)] for _ in range(n)]
+    # 그냥 한 명씩 움직이기
+    for i in range(1, m + 1):
+        if not player_loc[i]:
+            continue
+        r, c = player_loc[i]
+        d = player_dir[i]
 
-
-def next_pos(x, y, curr_dir):
-    dxs, dys = [-1, 1, 0, 0], [0, 0, -1, 1]
-    num, _ = player[x][y]
-
-    # Case 1.
-    # 먼저 독점계약을 맺지 않은 공간이 있다면 
-    # 우선순위에 따라 그곳으로 이동합니다.
-    for move_dir in next_dir[num][curr_dir]:
-        nx, ny = x + dxs[move_dir], y + dys[move_dir]
-        
-        if can_go(nx, ny, EMPTY_NUM):
-            return (nx, ny, move_dir)
-    
-    # Case 2.
-    # 인접한 곳이 모두 독점계약을 맺은 곳이라면
-    # 우선순위에 따라 그 중 본인이 독점계약한 땅으로 이동합니다.
-    for move_dir in next_dir[num][curr_dir]:
-        nx, ny = x + dxs[move_dir], y + dys[move_dir]
-        
-        if can_go(nx, ny, num):
-            return (nx, ny, move_dir)
-
-
-# (x, y) 위치에 새로운 플레이어가 들어왔을 때 갱신을 진행합니다.
-def update(x, y, new_player):
-    # 새로 들어온 플레이어가 더 우선순위가 높을 경우에만
-    # (x, y)위치에 해당 플레이어가 위치하게 됩니다.
-    # Tip.
-    # Empty인 위치에서는 항상 update가 되게끔
-    # 미리 Empty의 num 값에 401를 셋팅해놨습니다.
-    if next_player[x][y] > new_player:
-        next_player[x][y] = new_player
-
-
-def move(x, y):
-    num, curr_dir = player[x][y]
-    
-    # Step1. 현재 플레이어의 다음 위치와 방향을 구합니다.
-    nx, ny, move_dir = next_pos(x, y, curr_dir)
-    
-    # Step2. 플레이어를 옮겨줍니다.
-    update(nx, ny, (num, move_dir))
-
-
-def dec_contract(x, y):
-    num, remaining_period = contract[x][y]
-    
-    # 남은 기간이 1이면 다시 Empty가 됩니다.
-    if remaining_period == 1:
-        contract[x][y] = EMPTY
-    # 그렇지 않다면 기간이 1 줄어듭니다.
-    else:
-        contract[x][y] = (num, remaining_period - 1)
-
-
-def add_contract(x, y):
-    num, _ = player[x][y];
-    contract[x][y] = (num, k)
-
-
-def simulate():
-    # Step1. next_player를 초기화합니다.
-    for i in range(n):
-        for j in range(n):
-            next_player[i][j] = EMPTY
-    
-    # Step2. 각 플레이어들을 한 칸씩 움직여줍니다.
-    for i in range(n):
-        for j in range(n):
-            if player[i][j] != EMPTY:
-                move(i, j)
-
-    # Step3. next_grid 값을 grid로 옮겨줍니다.
-    for i in range(n):
-        for j in range(n):
-            player[i][j] = next_player[i][j]
-    
-    # Step4. 남은 contract기간을 1씩 감소시킵니다.
-    for i in range(n):
-        for j in range(n):
-            if contract[i][j] != EMPTY:
-                dec_contract(i, j)
-    
-    # Step5. 새로운 contract를 갱신해줍니다.
-    for i in range(n):
-        for j in range(n):
-            if player[i][j] != EMPTY:
-                add_contract(i, j)
-
-
-def end():
-    if elapsed_time >= 1000:
-        return True
-    
-    for i in range(n):
-        for j in range(n):
-            if player[i][j] == EMPTY:
+        adj_monopoly_list = 0
+        check_empty_space = False
+        # 첫번째 조건에서는 갈 수 있는 빈 땅이 있는 지만 체크한다.
+        for nd in each_player_d[i][d]:
+            nr, nc = r + dr[nd], c + dc[nd]
+            # 못 가는 조건 : 격자 벗어나거나, 내 땅이 아니거나, 상대방이 있는 경우
+            if not (0 <= nr < n and 0 <= nc < n):
                 continue
-            
-            num, _ = player[i][j]
-            
-            if num != 1:
-                return False
-    
-    return True
+            if not monopoly[nr][nc]:
+                new_grid[nr][nc].append(i)
+                player_loc[i] = (nr, nc)
+                player_dir[i] = nd
+                break
+            elif monopoly[nr][nc][0] == i and not adj_monopoly_list:
+                adj_monopoly_list = (nr, nc, nd)
+        else:
+            # 빈 공간을 찾았었는지 확인
+            nr, nc, nd = adj_monopoly_list
+            new_grid[nr][nc].append(i)
+            player_loc[i] = (nr, nc)
+            player_dir[i] = nd
+
+    return new_grid
 
 
-# 플레이어 마다 초기 방향을 입력받아 설정해줍니다.
-init_dirs = list(map(int, input().split()))
-for num, move_dir in enumerate(init_dirs, start=1):
+def remove_players():
+    global total_player
     for i in range(n):
         for j in range(n):
-            if given_map[i][j] == num:
-                player[i][j] = (num, move_dir - 1)
-                contract[i][j] = (num, k)
+            if len(grid[i][j]) > 1:
+                while len(grid[i][j]) > 1:
+                    player = grid[i][j].pop()
+                    player_loc[player] = 0
+                    player_dir[player] = 0
+                    total_player -= 1
 
 
-# 플레이어 마다 방향 우선순위를 설정합니다.
-for num in range(1, m + 1):
-    for curr_dir in range(DIR_NUM):
-        dirs = list(map(int, input().split()))
-        for i, move_dir in enumerate(dirs):
-            next_dir[num][curr_dir][i] = move_dir - 1
+def register_monopoly():
+    for i in range(n):
+        for j in range(n):
+            if grid[i][j]:
+                monopoly[i][j] = (grid[i][j][0], k)
+            elif monopoly[i][j]:
+                p, t = monopoly[i][j]
+                t -= 1
+                if not t:
+                    monopoly[i][j] = 0
+                else:
+                    monopoly[i][j] = (p, t)
 
-# 시간이 1000이 넘지 않고
-# 1번이 아닌 플레이어가 남아 있다면
-# 계속 시뮬레이션을 반복합니다.
-while not end():
-    simulate()
-    elapsed_time += 1
 
-if elapsed_time >= 1000:
-    elapsed_time = -1
+n, m, k = map(int, input().split())
+grid_input = [list(map(int, input().split())) for _ in range(n)]
+player_dir = [0] + list(map(int, input().split()))
+each_player_d = [0]
+for _ in range(m):
+    each_player_d.append([0] + [list(map(int, input().split())) for _ in range(4)])
+monopoly = [[0] * n for _ in range(n)]
+grid = [[[] * n for _ in range(n)] for _ in range(n)]
 
-print(elapsed_time)
+player_loc = [0] * (m + 1)
+for i in range(n):
+    for j in range(n):
+        if grid_input[i][j]:
+            player_loc[grid_input[i][j]] = (i, j)
+            grid[i][j].append(grid_input[i][j])
+            monopoly[i][j] = (grid_input[i][j], k)
+
+total_player = m
+ans = 0
+while ans < 1000:
+    # 1. 플레이어 이동
+    grid = move_players()
+
+    # 2. 겹치는 부분 제거
+    remove_players()
+
+    # 3. 모노폴리에 등록
+    register_monopoly()
+
+    ans += 1
+    if total_player == 1:
+        break
+
+if ans >= 1000:
+    print(-1)
+else:
+    print(ans)
